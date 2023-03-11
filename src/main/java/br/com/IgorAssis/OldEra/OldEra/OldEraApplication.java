e923cd3d2b19649ba57501c28cb8f5873c3871c7
yurediniz <yurediniz@hotmail.com>
2023-03-11 00:37:53 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/IgorAssis/OldEra/OldEra/OldEraApplication.java
@@ -0,0 +1,12 @@
+package br.com.IgorAssis.OldEra.OldEra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldEraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldEraApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/IgorAssis/OldEra/OldEra/OldEraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.IgorAssis.OldEra.OldEra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldEraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
