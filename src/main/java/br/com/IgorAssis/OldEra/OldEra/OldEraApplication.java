fdcf536cdea26867c259ef26220415973843975e
yurediniz <yurediniz@hotmail.com>
2023-03-11 00:37:53 -0300

    OldEra Rebase

--- src/main/java/br/com/IgorAssis/OldEra/OldEra/OldEraApplication.java
+++ src/main/java/br/com/IgorAssis/OldEra/OldEra/OldEraApplication.java
@@ -2,10 +2,8 @@ package br.com.IgorAssis.OldEra.OldEra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class OldEraApplication {
 
 	public static void main(String[] args) {
