e5d3615c90721fd51051609280e4c59a2a8e8152
yurediniz <yurediniz@hotmail.com>
2023-03-24 05:10:07 -0300

    Remoção da pasta duplicada OldEra, novo caminho: \src\main\java\br\IgorAssis\OldEra

--- src/main/java/br/com/IgorAssis/OldEra/OldEra/Enums/RoleName.java
+++
@@ -1,6 +0,0 @@
-package br.com.IgorAssis.OldEra.OldEra.Enums;
-
-public enum RoleName {
-    ROLE_ADMIN,
-    ROlE_USER
-}
--- src/main/java/br/com/IgorAssis/OldEra/OldEra/Repository/VendedorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.IgorAssis.OldEra.OldEra.Repository;
-
-import br.com.IgorAssis.OldEra.OldEra.Entity.Vendedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
-}
