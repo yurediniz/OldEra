43b6b23df52bd4a7473078d9790332c0aa2bcd9e
yurediniz <yurediniz@hotmail.com>
2023-03-25 23:53:28 -0300

    Configuração do Swaggger

--- src/main/java/br/com/IgorAssis/OldEra/OldEra/Entity/Cliente.java
+++ src/main/java/br/com/IgorAssis/OldEra/OldEra/Entity/Cliente.java
@@ -1,20 +1,24 @@
 package br.com.IgorAssis.OldEra.OldEra.Entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+import java.io.Serializable;
 
 @Entity
-public class Cliente {
+@Data
+public class Cliente implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
     private String email;
     private String endereco;
-
-    // construtores, getters e setters
 
 }
---
+++ src/main/java/br/com/IgorAssis/OldEra/OldEra/Repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package br.com.IgorAssis.OldEra.OldEra.Repository;
+
+import br.com.IgorAssis.OldEra.OldEra.Entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Query("SELECT c FROM Cliente c WHERE c.nome LIKE %:nome%")
+    List<Cliente> buscarClientesPorNome(@Param("nome") String nome);
+
+    @Query(value = "SELECT COUNT(*) FROM cliente WHERE email = :email", nativeQuery = true)
+    Integer contarClientesPorEmail(@Param("email") String email);
+}
