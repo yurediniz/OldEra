9e85505f1c0b88a4639f9d4fc652757fe1efd175
Yure Diniz <yurediniz@hotmail.com>
2023-05-17 01:44:53 -0300

    moved

--- src/main/java/br/com/IgorAssis/OldEra/OldEra/OldEraApplication.java
+++ src/main/java/br/com/IgorAssis/OldEra/OldEra/OldEraApplication.java
@@ -1,4 +1,4 @@
-package br.com.IgorAssis.OldEra;
+package br.com.IgorAssis.OldEra.OldEra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
