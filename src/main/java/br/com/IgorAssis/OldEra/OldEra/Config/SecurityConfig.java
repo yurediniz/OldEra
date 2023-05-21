e3ec5e3ae7b0d44f8382ec4d65e21f42b2c0b65e
Yure Diniz <yurediniz@hotmail.com>
2023-05-21 00:12:54 -0300

    Config do JWT

--- src/main/java/br/com/IgorAssis/OldEra/OldEra/Config/SecurityConfig.java
+++ src/main/java/br/com/IgorAssis/OldEra/OldEra/Config/SecurityConfig.java
@@ -1,27 +1,39 @@
 package br.com.IgorAssis.OldEra.OldEra.Config;
 
+import br.com.IgorAssis.OldEra.OldEra.Token.FilterToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    private FilterToken filter;
+
     @Bean
-    protected SecurityFilterChain filtreChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .csrf().disable();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests()
+                .antMatchers("/auth/login")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
